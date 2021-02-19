5783ca4b923808ead642454e4e5bb858f9c3213c
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-19 21:51:17 +0000

    reducers working as intended

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import loggedReducer from './isLogged';
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
-    counter: counterReducer,
-    isLogged: loggedReducer
+    counterReducer,
+    loggedReducer
 })
 
 export default allReducers;
