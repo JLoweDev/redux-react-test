04b1bf4bb85b370c6b85de04999c5cf5e3fc184c
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-19 22:06:28 +0000

    Increment and Decrement now work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import counterReducer from './reducers/counter';
+import { increment, decrement } from './actions'
 
 function App() {
   const counter = useSelector(state => state.counterReducer)
   const isLogged = useSelector(state => state.loggedReducer)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       <>
       {isLogged ? <h3>Confidential</h3> : <h3>Please Log In</h3>}
       </>
