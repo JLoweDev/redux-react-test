dc90c150f8a73457d6d4906b40d1a22119e38dc6
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-19 21:58:32 +0000

    added a ternary operator for logged in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import counterReducer from './reducers/counter';
 
 function App() {
+  const counter = useSelector(state => state.counterReducer)
+  const isLogged = useSelector(state => state.loggedReducer)
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Counter: {counter}</h1>
+      <>
+      {isLogged === true ? <h3>Confidential</h3> : <h3>Please Log In</h3>}
+      </>
     </div>
   );
 }
