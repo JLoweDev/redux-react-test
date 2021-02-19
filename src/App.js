758bc5ba684d0a8f2b9af0d3457975e8661acc95
James <67014970+JLoweDev@users.noreply.github.com>
2021-02-19 22:11:08 +0000

    added payload and number to input

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment(2))}>+</button>
+      <button onClick={() => dispatch(decrement(3))}>-</button>
       <>
       {isLogged ? <h3>Confidential</h3> : <h3>Please Log In</h3>}
       </>
